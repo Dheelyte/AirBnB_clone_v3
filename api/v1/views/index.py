52ebf4e130ba7cd555516708fd18793766c7fccc
Delight Olu-Olagbuji <olagbujidelight@gmail.com>
2023-08-01 00:33:40 +0000

    Add docstring and shebang

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,11 @@
+#!/usr/bin/python3
+"""
+Create an endpoint that retrieves the number of each objects by type:
+"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+
 
 
 @app_views.route("/status")
@@ -7,6 +13,24 @@ def status():
     """
     Create JSON status
     """
-    return jsonify({
-        'status': 'OK'
-        })
+    return jsonify(
+        {
+            'status': 'OK'
+        }
+    )
+
+
+@app_views.route("/stats")
+def storage_counts():
+    '''
+        return counts of all classes in storage
+    '''
+    cls_counts = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(cls_counts) 
