4bb33df3f9e569346dc08a4e36bc00731eae1778
Delight Olu-Olagbuji <olagbujidelight@gmail.com>
2023-08-01 00:43:02 +0000

    Add 404 handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,12 +10,21 @@ from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def tear_down(self):
     """
     Close query after each session
     """
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    '''
+    return JSON formatted 404 status code response
+    '''
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
